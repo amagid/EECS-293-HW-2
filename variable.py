ce900a9f8def62a052ce9fa65f0cc50a7c05362d
Aaron Magid <ahm64@case.edu>
2019-01-29 10:30:11 -0500

    feat(Variables): Added variable building and caching functionality.

--- variable.py
+++ variable.py
@@ -3,8 +3,17 @@ Variable class represents an Abstract Token which is specifically a variable.
 '''
 from token import TerminalSymbol
 from abstract_token import AbstractToken
+from cache import Cache
 
 class Variable(AbstractToken):
+
+    # Static cache to prevent creation of duplicate variables
+    _cache = Cache()
+
+    # Static method to build and return a new Variable with the given name (representation)
+    @staticmethod
+    def build(representation):
+        return Variable._cache.get(representation, Variable)
 
     def __init__(self, representation):
 
@@ -16,6 +25,10 @@ class Variable(AbstractToken):
         self._type = TerminalSymbol.VARIABLE
         self._representation = representation
 
-    # Get and return the name of this variable
+    # Override __str__ to return this Variable's name (representation)
+    def __str__(self):
+        return self._representation
+
+    # Get and return the name of this Variable
     def representation(self):
         return self._representation
