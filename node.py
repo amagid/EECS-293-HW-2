2739348f342e06a83ab2f05ea4ae3f73624e23c5
Aaron Magid <ahm64@case.edu>
2019-01-29 11:39:56 -0500

    feat(Nodes): First pass at node base class.

---
+++ node.py
@@ -0,0 +1,13 @@
+'''
+Node base class
+'''
+
+class Node():
+    
+    # Blank init method since thie class should not be directly instantiated
+    def __init__(self):
+        pass
+
+    # toList method returns subtree under this node as a Collection
+    def toList(self):
+        pass
