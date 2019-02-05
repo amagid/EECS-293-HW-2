1685faef226dd090973eaf7ab58a4824a5d78ffb
Aaron Magid <ahm64@case.edu>
2019-02-04 22:52:57 -0500

    feat(HW2Corrections): Changed try-except control flow to if-else in cache.py.

--- cache.py
+++ cache.py
@@ -8,14 +8,20 @@ class Cache():
     def __init__(self):
         self._cache = {}
 
+    # TA: Is there a better way I can organize this method?
+    # I'd rather have fewer conditionals, but I'd also rather 
+    # not sandwich the nominal case in between error cases by
+    # moving the "if key is None" up into a guard clause.
     def get(self, key, constructor):
-        try:
+
+        if key in self._cache:
             return self._cache[key]
-        except KeyError:
-            if key is None:
-                raise ValueError("No key passed")
-            elif constructor is None:
-                raise ValueError("Key not found, and no constructor passed to create new object")
-            
+        elif constructor is not None:
             self._cache[key] = constructor(key)
             return self._cache[key]
+
+        if key is None:
+            raise ValueError("No key passed")
+        else:
+            raise ValueError("Key not found, and no constructor passed to create new object")
+            
