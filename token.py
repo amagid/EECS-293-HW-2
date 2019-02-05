363b810ede5166914314d7fc9a331ac20bfc1153
Aaron Magid <ahm64@case.edu>
2019-02-04 22:14:00 -0500

    feat(HW2Corrections): Updated TerminalSymbol to be an enum.

--- token.py
+++ token.py
@@ -3,16 +3,17 @@ Main Token storage file, including general Token class and a makeshift
 TerminalSymbol enum since as far as I'm aware, Python does not have enums.
 '''
 
-# TerminalSymbol dictionary
-TerminalSymbol = {
-    'VARIABLE': 1,
-    'PLUS': 2,
-    'MINUS': 3,
-    'TIMES': 4,
-    'DIVIDE': 5,
-    'OPEN': 6,
-    'CLOSE': 7
-}
+from enum import Enum
+# TerminalSymbol enum
+class TerminalSymbol(Enum):
+    VARIABLE = 1
+    PLUS = 2
+    MINUS = 3
+    TIMES = 4
+    DIVIDE = 5
+    OPEN = 6
+    CLOSE = 7
+
 
 class Token():
 
