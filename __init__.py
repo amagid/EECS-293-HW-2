a40681e0cd7b12c1e40fcbecf28aeef9e959fe87
Aaron Magid <ahm64@case.edu>
2019-02-04 22:44:32 -0500

    feat(HW2Corrections): Made this whole thing into a package.

---
+++ __init__.py
@@ -0,0 +1,14 @@
+__all__ = [
+    'abstract_token'
+    'cache',
+    'connector',
+    'internal_node',
+    'leaf_node',
+    'node',
+    'terminal_symbol',
+    'token',
+    'variable'
+]
+
+def main():
+    print('ran init')
---
+++ __main__.py
@@ -0,0 +1,7 @@
+'''
+Entry point for application. Just runs main() function in __init__.py
+'''
+
+from __init__ import main
+
+main()
