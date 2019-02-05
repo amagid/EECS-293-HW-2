da78093821dc5d23e98eab8ec555b335da11d488
Aaron Magid <ahm64@case.edu>
2019-02-04 21:59:51 -0500

    feat(HW2Corrections): Made AbstractToken class actually abstract.

--- abstract_token.py
+++ abstract_token.py
@@ -1,10 +1,10 @@
 '''
 Abstract Token class extends and fully implements the Token base class.
 '''
-
+from abc import ABC, abstractmethod
 from token import Token, TerminalSymbol
 
-class AbstractToken(Token):
+class AbstractToken(ABC, Token):
 
     # Init defines _type attribute
     def __init__(self):
@@ -13,9 +13,11 @@ class AbstractToken(Token):
         self._type = None
 
     # Retrieve and return this Token's type
+    # Intentionally not abstract method since it is the same for all subclasses
     def get_type(self):
         return self._type
 
     # Check whether this Token matches the supplied type, and return the result
+    # Intentionally not abstract method since it is the same for all subclasses
     def matches(self, token_type):
         return token_type == self._type
