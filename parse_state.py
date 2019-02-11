5bb6c22da75ce95e4e15fb4c2ccf443cb4a70ebd
Aaron Magid <ahm64@case.edu>
2019-02-11 01:00:42 -0500

    feat(ParseState): Initial pass at ParseState fields and getters.

--- parse_state.py
+++ parse_state.py
@@ -1,2 +1,30 @@
 class ParseState():
-    pass
+
+    _FAILURE = None
+
+    @staticmethod
+    def create_failure_state():
+        failure = ParseState(None, None)
+        failure._success = False
+        return failure
+
+    def __init__(self, node, remainder):
+        self._success = True
+        self._node = node
+        self._remainder = remainder
+
+        # Create FAILURE state if it doesn't already exist
+        if ParseState._FAILURE is None:
+            ParseState._FAILURE = ParseState.create_failure_state()
+
+    def success(self):
+        return self._success
+
+    def node(self):
+        return self._node
+
+    def remainder(self):
+        return self._remainder
+
+    def has_no_remainder(self):
+        return len(self._remainder) == 0
