4ddb22c3e0105b797a11042b73ed75fd04ce0552
Aaron Magid <ahm64@case.edu>
2019-02-11 13:33:26 -0500

    feat(TerminalSymbol): Updated TerminalSymbol enum to use new TerminalSymbol static instances.

--- terminal_symbol.py
+++ terminal_symbol.py
@@ -7,13 +7,13 @@ from parse_state import ParseState
 
 # TerminalSymbol enum
 class TerminalSymbol(Symbol):
-    VARIABLE = 1
-    PLUS = 2
-    MINUS = 3
-    TIMES = 4
-    DIVIDE = 5
-    OPEN = 6
-    CLOSE = 7
+    VARIABLE = None
+    PLUS = None
+    MINUS = None
+    TIMES = None
+    DIVIDE = None
+    OPEN = None
+    CLOSE = None
 
     def __init__(self, terminal_type):
         self._type = terminal_type
@@ -24,3 +24,74 @@ class TerminalSymbol(Symbol):
             return ParseState.build(LeafNode.build(token_list[0]), token_list[1:])
         else:
             return ParseState.FAILURE
+
+# TA: Is there a better way to do this? I couldn't get the static
+# variables assigned any other way (not cleanly at least).
+# Create VARIABLE symbol
+def _create_variable_symbol():
+    # Block duplicate runs
+    if TerminalSymbol.VARIABLE is not None:
+        return
+
+    TerminalSymbol.VARIABLE = TerminalSymbol(1)
+    
+# Create PLUS symbol
+def _create_plus_symbol():
+    # Block duplicate runs
+    if TerminalSymbol.PLUS is not None:
+        return
+
+    TerminalSymbol.PLUS = TerminalSymbol(2)
+    
+# Create MINUS symbol
+def _create_minus_symbol():
+    # Block duplicate runs
+    if TerminalSymbol.MINUS is not None:
+        return
+
+    TerminalSymbol.MINUS = TerminalSymbol(3)
+    
+# Create TIMES symbol
+def _create_times_symbol():
+    # Block duplicate runs
+    if TerminalSymbol.TIMES is not None:
+        return
+
+    TerminalSymbol.TIMES = TerminalSymbol(4)
+    
+# Create DIVIDE symbol
+def _create_divide_symbol():
+    # Block duplicate runs
+    if TerminalSymbol.DIVIDE is not None:
+        return
+
+    TerminalSymbol.DIVIDE = TerminalSymbol(5)
+    
+# Create OPEN symbol
+def _create_open_symbol():
+    # Block duplicate runs
+    if TerminalSymbol.OPEN is not None:
+        return
+
+    TerminalSymbol.OPEN = TerminalSymbol(6)
+    
+# Create CLOSE symbol
+def _create_close_symbol():
+    # Block duplicate runs
+    if TerminalSymbol.CLOSE is not None:
+        return
+
+    TerminalSymbol.CLOSE = TerminalSymbol(7)
+
+# Create all static symbols
+def _create_constant_symbols():
+    _create_variable_symbol()
+    _create_plus_symbol()
+    _create_minus_symbol()
+    _create_times_symbol()
+    _create_divide_symbol()
+    _create_open_symbol()
+    _create_close_symbol()
+
+# Create all static symbols on module load
+_create_constant_symbols()
