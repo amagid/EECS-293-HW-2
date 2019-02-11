c3fefe2209f14af2a1860e4dda01e73b66ce8827
Aaron Magid <ahm64@case.edu>
2019-02-10 21:01:14 -0500

    test(LeafNode): Tests for LeafNode class.

--- test_leaf_node.py
+++ test_leaf_node.py
@@ -1,5 +1,17 @@
+import pytest
 from leaf_node import LeafNode
+from connector import Connector
+from terminal_symbol import TerminalSymbol
 
-def test_the_tester():
-    print('tested')
-    assert True
+def test_error_on_no_token():
+    with pytest.raises(ValueError):
+        LeafNode.build(None)
+
+def test_str_with_connector():
+    ln = LeafNode(Connector.build(TerminalSymbol.PLUS))
+    assert str(ln) == '+'
+
+def test_to_list_with_connector():
+    conn = Connector.build(TerminalSymbol.PLUS)
+    ln = LeafNode(conn)
+    assert ln.toList() == [conn]
