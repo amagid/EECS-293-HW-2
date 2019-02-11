997495ce963ac73362e0664d8cfa2f31807b55e6
Aaron Magid <ahm64@case.edu>
2019-02-11 01:02:55 -0500

    chore(Documentation): Function documentation for ParseState.

--- parse_state.py
+++ parse_state.py
@@ -1,13 +1,16 @@
 class ParseState():
 
+    # Static FAILURE state
     _FAILURE = None
 
+    # Generates the FAILURE ParseState
     @staticmethod
     def create_failure_state():
         failure = ParseState(None, None)
         failure._success = False
         return failure
 
+    # Init takes arguments for current processed tree and remaining Token list
     def __init__(self, node, remainder):
         self._success = True
         self._node = node
@@ -17,14 +20,18 @@ class ParseState():
         if ParseState._FAILURE is None:
             ParseState._FAILURE = ParseState.create_failure_state()
 
+    # Getter for self._success
     def success(self):
         return self._success
 
+    # Getter for self._node
     def node(self):
         return self._node
 
+    # Getter for self._remainder
     def remainder(self):
         return self._remainder
 
+    # Check whether there is no remainder. Returns True if remainder list is empty
     def has_no_remainder(self):
         return len(self._remainder) == 0
