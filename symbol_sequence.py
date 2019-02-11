cc7d52bb9f1018a8168489e15a3690e0020bd0fe
Aaron Magid <ahm64@case.edu>
2019-02-11 10:52:05 -0500

    feat(SymbolSequence): First pass at SymbolSequence.match().

--- symbol_sequence.py
+++ symbol_sequence.py
@@ -1,3 +1,6 @@
+from parse_state import ParseState
+from internal_node import InternalNode
+
 class SymbolSequence():
 
     EPSILON = None
@@ -18,6 +21,30 @@ class SymbolSequence():
 
     def __str__(self):
         return str(self._production)
+
+    def match(self, token_list):
+        # Guard against None token_list
+        if token_list is None:
+            raise ValueError('SymbolSequence cannot match to None token_list')
+
+        # Track remainder and children
+        remainder = token_list
+        children = []
+
+        # Attempt to parse each Token in the list
+        for prod_symbol in self._production:
+            state = prod_symbol.parse(remainder)
+
+            # Handle unsuccessful parse
+            if not state.success():
+                return ParseState.FAILURE
+
+            # else add node to children and update remainder
+            children.append(state.node())
+            remainder = state.remainder()
+        
+        # Return a ParseState containing a new InternalNode for the root and the remainder
+        return ParseState.build(InternalNode.build(children), remainder)
 
 # TA: Is there a better way to do this? I couldn't get the EPSILON static
 # variable assigned any other way (not cleanly at least).
