b09329ff5b57272f5f777eeaaf5def7c90235d5e
Aaron Magid <ahm64@case.edu>
2019-02-10 20:05:35 -0500

    test(Cache): Changed string generator to actually use its argument.

--- test_cache.py
+++ test_cache.py
@@ -4,8 +4,8 @@ from cache import Cache
 def _create_cache():
     return Cache()
 
-def _create_abc123_string(key_to_ignore):
-    return 'abc123'
+def _create_string(string):
+    return 'abc' + string
 
 def test_error_on_no_key_supplied():
     _cache = _create_cache()
@@ -19,10 +19,10 @@ def test_error_on_no_constructor_supplied():
 
 def test_creates_new_instance():
     _cache = _create_cache()
-    test_string = _cache.get('test', _create_abc123_string)
-    assert test_string == 'abc123'
+    test_string = _cache.get('test', _create_string)
+    assert test_string == 'abctest'
 
 def test_retrieves_existing_instance():
     _cache = _create_cache()
-    test_string = _cache.get('test', _create_abc123_string)
-    assert test_string is _cache.get('test', _create_abc123_string)
+    test_string = _cache.get('test', _create_string)
+    assert test_string is _cache.get('test', _create_string)
