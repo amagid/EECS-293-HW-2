9655edd81e623e1ff80fa3b37f960b0205a8c370
Aaron Magid <ahm64@case.edu>
2019-02-11 14:57:28 -0500

    fix(AbstractToken): Fixed matches() method to check TerminalSymbol object, not underlying type number.

--- abstract_token.py
+++ abstract_token.py
@@ -20,4 +20,5 @@ class AbstractToken(Token):
     # Check whether this Token matches the supplied type, and return the result
     # Intentionally not abstract method since it is the same for all subclasses
     def matches(self, token_type):
-        return token_type == self._type
+        print('matches: ' + str(token_type) + ', ' + str(self._type))
+        return token_type is self._type
