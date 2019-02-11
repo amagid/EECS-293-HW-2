beaec1e1bd80592545f0e16f834f2f89585730a7
Aaron Magid <ahm64@case.edu>
2019-02-10 20:18:54 -0500

    test(Connector): Tests for Connector class.

--- test_connector.py
+++ test_connector.py
@@ -1,5 +1,22 @@
+import pytest
 from connector import Connector
+from terminal_symbol import TerminalSymbol
 
-def test_the_tester():
-    print('tested')
-    assert True
+def test_error_on_no_token_type():
+    with pytest.raises(ValueError):
+        Connector.build(None)
+        
+def test_error_on_variable_token_type():
+    with pytest.raises(ValueError):
+        Connector.build(TerminalSymbol.VARIABLE)
+
+def test_connector_caching():
+    conn = Connector.build(TerminalSymbol.PLUS)
+    assert conn is Connector.build(TerminalSymbol.PLUS)
+
+def test_connector_type_matches():
+    conn = Connector.build(TerminalSymbol.MINUS)
+    assert Connector.matches(conn, TerminalSymbol.MINUS)
+
+def test_connector_str():
+    assert str(Connector.build(TerminalSymbol.DIVIDE)) == '/'
