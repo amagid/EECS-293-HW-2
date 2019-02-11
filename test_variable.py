4b25e46cfe0d8eeed9d17def30b082713affc2d7
Aaron Magid <ahm64@case.edu>
2019-02-10 21:15:34 -0500

    test(Variable): Tests for Variable class.

--- test_variable.py
+++ test_variable.py
@@ -1,5 +1,20 @@
+import pytest
 from variable import Variable
+from terminal_symbol import TerminalSymbol
 
-def test_the_tester():
-    print('tested')
-    assert True
+def test_error_on_no_representation():
+    with pytest.raises(ValueError):
+        Variable(None)
+
+def test_variable_caching():
+    var = Variable.build('x')
+    assert Variable.build('x') is var
+
+def test_variable_type_matches():
+    assert Variable.build('x').matches(TerminalSymbol.VARIABLE)
+
+def test_variable_str():
+    assert str(Variable.build('x')) == 'x'
+
+def test_variable_representation():
+    assert Variable.build('x').representation() == 'x'
