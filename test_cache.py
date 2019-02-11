2b998ce96d4e783809293b783d2a46486ba987db
Aaron Magid <ahm64@case.edu>
2019-02-10 19:51:08 -0500

    test(Cache): testing for Cache class.

--- test_cache.py
+++ test_cache.py
@@ -1,5 +1,25 @@
+import pytest
 from cache import Cache
 
-def test_the_tester():
-    print('tested')
-    assert True
+@pytest.fixture
+def cache():
+    return Cache()
+
+def test_error_on_no_key_supplied(cache);
+    with pytest.raises(ValueError):
+        cache.get(None)
+
+def test_error_on_no_constructor_supplied(cache);
+    with pytest.raises(ValueError):
+        cache.get('test', None)
+
+def test_creates_new_instance(cache):
+    test_string = cache.get('test', _create_abc123_string)
+    assert test_string == 'abc123', 'Cache must create new object with provided constructor'
+
+def test_retrieves_existing_instance(cache):
+    test_string = cache.get('test', _create_abc123_string)
+    assert test_string is cache.get('test', _create_abc123_string), 'Cache must return existing objects'
+
+def _create_abc123_string():
+    return 'abc123'
