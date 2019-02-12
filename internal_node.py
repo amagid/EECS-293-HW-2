61fdd94dedf94104386cf6f7d1e30356bfce3773
Aaron Magid <ahm64@case.edu>
2019-02-11 23:48:15 -0500

    feat(Simplification): First pass at Builder.simplify().

--- internal_node.py
+++ internal_node.py
@@ -9,12 +9,48 @@ class InternalNode(Node):
 
     # Builder nested class
     class Builder():
+
         def __init__(self):
             self._children = []
 
         # Remove childless nodes and collapse single-child nodes
         def simplify(self):
-            pass
+            for child in self._children:
+                self._simplify_node(child)
+
+            return self
+
+        def _simplify_node(self, node):
+            children = node.get_children()
+
+            # If this node is a leaf node, return the node
+            if children is None:
+                return node
+            # If this node has no children, delete it
+            elif len(children) == 0:
+                return None
+            # If this node has one child, replace this node with its child & recurse
+            elif len(children) == 1:
+                return self._simplify_node(children[0])
+
+            # If this node has more than one child, recurse on children
+            for i in range(0, len(children)):
+                children[i] = self._simplify_node(children[i])
+
+            # Remove children from list if they are None
+            self._collapse_none_children(node)
+
+            # Return the node so it can be added to the parent's _children list
+            return node
+
+        def _collapse_none_children(self, node):
+            output = []
+            for child in node.get_children():
+                if child is not None:
+                    output.append(child)
+
+            node._children = output
+
 
         # Convert to InternalNode
         def build(self):
