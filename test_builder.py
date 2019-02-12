58bfd0afd10a69c17e55c9079ee23c3150ab8063
Aaron Magid <ahm64@case.edu>
2019-02-12 01:55:27 -0500

    test(Builder): Tests for add_child().

--- test_builder.py
+++ test_builder.py
@@ -5,36 +5,95 @@ from test_non_terminal_symbol import _str_to_token_list
 from non_terminal_symbol import NonTerminalSymbol
 
 # Test add_child actually adds the child
+def test_add_child_adds_child():
+    b = Builder()
+    node = InternalNode.build([])
+    b.add_child(node)
+
+    assert b._children[0] is node
+
 # Test add_child returns True
+def test_add_child_returns_true():
+    b = Builder()
+    node = InternalNode.build([])
+    result = b.add_child(node)
+
+    assert result
 
 # Test _post_process_node with no children returns None
+
+
 # Test _post_process_node with one child returns that child simplified
+
+
 # Test _post_process_node with a LeafNode returns the LeafNode
+
+
 # Test _post_process_node with many children returns the given Node
 
+
+
 # Test _collapse_none_children with no children returns empty list
+
+
 # Test _collapse_none_children with one child returns list containing the child
+
+
 # Test _collapse_none_children with many children and Nones returns only children in order
 
+
+
 # Test build with no children returns empty InternalNode
+
+
 # Test build with one LeafNode child returns the LeafNode
+
+
 # Test build with one empty InternalNode child returns an empty InternalNode
+
+
 # Test build with InternalNode that has one LeafNode child  returns the LeafNode
+
+
 # Test build with InternalNode that has many LeafNode children returns an InternalNode with those children
+
+
 
 # Test _simplify_node with a LeafNode returns the LeafNode
+
+
 # Test _simplify_node with many children returns node with those children simplified
 
+
+
 # Test simplify acts on all children
+
+
 # Test simplify removes None children at end
+
+
 # Test simplify does nothing when no children present
+
+
 # Test simplify returns the given Builder
 
+
+
 # Test build with no children returns empty InternalNode
+
+
 # Test build with one LeafNode child returns the LeafNode
+
+
 # Test build with one empty InternalNode child returns an empty InternalNode
+
+
 # Test build with InternalNode that has one LeafNode child  returns the LeafNode
+
+
 # Test build with InternalNode that has many LeafNode children returns an InternalNode with those children
+
+
 # Test build on the assignment's given test expression, returns simplest possible string
 def test_builder_simplify():
     SIMPLE_STRING = '[a,[+,[b,[/,c]]]]'
