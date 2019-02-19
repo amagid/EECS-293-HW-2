2ab24389ce837bf58485f54faff68f38cbc37995
Aaron Magid <ahm64@case.edu>
2019-02-18 20:27:53 -0500

    fix(TerminalSymbol): Shaved down parse() to one return.

--- terminal_symbol.py
+++ terminal_symbol.py
@@ -17,14 +17,13 @@ class TerminalSymbol(Enum):
 
     # Parse the first Token in the list and return a ParseState, return FAILURE state if fails
     def parse(self, token_list):
-        # Guard against parsing empty expression
-        if len(token_list) == 0:
-            return FAILURE
-            
-        if token_list[0].matches(self):
-            return ParseState.build(LeafNode.build(token_list[0]), token_list[1:])
+        
+        if token_list != [] and token_list[0].matches(self):
+            state = ParseState.build(LeafNode.build(token_list[0]), token_list[1:])
         else:
-            return FAILURE
+            state = FAILURE
+
+        return state
 
     # Getter for internal _type field
     def get_type(self):
